2f53dc2a8bdc0b834f3cef9f05894d9db73ae49a
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-09-27 00:53:35 +0330

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncTestInWPF {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private readonly System.Timers.Timer timer;
+        private int progress = default(int);
+        public MainWindow() {
+            InitializeComponent();
+            timer = new System.Timers.Timer(500);
+            timer.Start();
+        }
+
+        private async void btnLongRunningProcess_Click(object sender, RoutedEventArgs e) {
+            progress = 1;
+            lblProgressReport.Content = "Progress started";
+            timer.Elapsed += Timer_Elapsed;
+            await Task.Run(() =>
+            {
+                // long running process (like merging contract PDFs)
+                Thread.Sleep(5000);
+            });
+            lblProgressReport.Content = "Done";
+            timer.Elapsed -= Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e) {
+            Dispatcher.BeginInvoke(() => lblProgressReport.Content = progress++);
+        }
+    }
+}
