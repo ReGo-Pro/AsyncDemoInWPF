37de5e9ea028158b3334078d58614b2ecea2b21e
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-12 19:33:45 +0330

    Add cancellation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,22 +8,36 @@ namespace AsyncTestInWPF {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        CancellationTokenSource cts;
+
         public MainWindow() {
             InitializeComponent();
+            cts = new CancellationTokenSource();
         }
 
         private async void btnLongRunningProcess_Click(object sender, RoutedEventArgs e) {
             // We don't even need dispatcher here
             IProgress<int> progress = new Progress<int>(i => lblProgressReport.Content = i.ToString());
             lblProgressReport.Content = "Progress started";
-            await Task.Run(() =>
-            {
-                for (int i = 0; i < 10; i++) {
-                    Thread.Sleep(1000);         // 10 cycles of a long-running operation
-                    progress.Report(i);
-                }
-            });
-            lblProgressReport.Content = "Done";
+
+            try {
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 10; i++) {
+                        Thread.Sleep(1000);         // 10 cycles of a long-running operation
+                        progress.Report(i);
+                        cts.Token.ThrowIfCancellationRequested();
+                    }
+                }, cts.Token);
+                lblProgressReport.Content = "Done";
+            }
+            catch (Exception ex) {
+                lblProgressReport.Content = "Canceled";
+            }
+        }
+
+        private void btnLongRunningProcessStop_Click(object sender, RoutedEventArgs e) {
+            cts.Cancel();
         }
     }
 }
